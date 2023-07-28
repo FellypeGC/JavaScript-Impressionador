f24ef2f88ebce461baa13410e436eeec5df47120
FellypeGC <99624115+FellypeGC@users.noreply.github.com>
2023-07-27 21:55:07 -0300

    Create declaracaoVariaveis.js

---
+++ modulo2/declaracaoVariaveis.js
@@ -0,0 +1,9 @@
+let variavelNova = 'valorDessaVariavel'
+variavelNova = 'novoValor'
+
+let ultimaVariavelDaAula
+
+ultimaVariavelDaAula = 'finalmente coloquei um calor nesta variavel'
+
+console.log(variavelNova)
+console.log(ultimaVariavelDaAula)
