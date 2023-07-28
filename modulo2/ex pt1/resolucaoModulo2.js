cf9ba46919282f9ce8e58a68855a49224fe967ed
FellypeGC <99624115+FellypeGC@users.noreply.github.com>
2023-07-28 10:42:56 -0300

    Create resolucaoModulo2.js

---
+++ modulo2/ex pt1/resolucaoModulo2.js
@@ -0,0 +1,30 @@
+console.log('Resultado do Exercício 1');
+
+const primeiraVariavel = 2148;
+let segundaVariavel;
+
+console.log(`O valor da primeira variável é ${primeiraVariavel}.`);
+
+segundaVariavel = 60;
+
+console.log(`Minha segunda variável vale ${segundaVariavel}`)
+console.log(`O valor da soma das duas variáveis é ${
+    primeiraVariavel + segundaVariavel
+    }`
+);
+segundaVariavel = segundaVariavel ** 2;
+console.log(segundaVariavel ** 2);
+console.log(segundaVariavel ** 2 / 3);
+
+console.log('Resultado do Exercício 2');
+
+const booleana1 = true;
+const booleana2 = false;
+
+console.log(`Operação AND as duas variáveis: ${booleana1 && booleana2}`);
+console.log(`Operação OR as duas variáveis: ${booleana1 || booleana2}`);
+
+const booleana3 = false;
+
+console.log(`Operação AND entre as três variáveis: ${booleana1 && booleana2 && booleana3}`);
+console.log(`Operação OR entre as três variáveis ${booleana1 || booleana2 || booleana3}`);
