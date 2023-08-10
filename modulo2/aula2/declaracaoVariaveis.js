5d2ac571a0ed435250d1ba655b61e8731865995f
FellypeGC <gabrielfellypesteve@gmail.com>
2023-08-10 00:51:09 -0300

    Update declaracaoVariaveis.js

--- modulo2/aula2/declaracaoVariaveis.js
+++ modulo2/aula2/declaracaoVariaveis.js
@@ -6,4 +6,4 @@ let ultimaVariavelDaAula
 ultimaVariavelDaAula = 'finalmente coloquei um calor nesta variavel'
 
 console.log(variavelNova)
-console.log(ultimaVariavelDaAula)
+console.log(ultimaVariavelDaAula) 
