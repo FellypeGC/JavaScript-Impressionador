c3ce31cd82b875ae149222392ccd72c8c118951d
FellypeGC <gabrielfellypesteve@gmail.com>
2023-08-14 22:34:31 -0300

    Create objeto.js

---
+++ modulo2/aula12/objeto.js
@@ -0,0 +1,12 @@
+const documentoIdentidade = {
+    nome: 'Jose',
+    sobrenome: 'da Silva',
+    cpf: '123.456.789-10',
+    empresa: 'Petrobras',
+    idade: 45,
+};
+
+// console.log(documentoIdentidade);
+console.log(documentoIdentidade.nome);
+console.log(documentoIdentidade.idade);
+console.log(documentoIdentidade["cpf"]);
