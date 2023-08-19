95c25fd066d93a4c210a233edc1b1f3ed9520c2b
FellypeGC <gabrielfellypesteve@gmail.com>
2023-08-18 22:27:24 -0300

    Create array.js

---
+++ modulo2/aula13/array.js
@@ -0,0 +1,13 @@
+const investimentoMensal = [1000, 2000, 3000, 2000, 1500, 1200, 1300];
+// const arrayDeStrings = ['string1', 2];
+
+// console.log(investimentoMensal);
+
+console.log(`O valor investido no mês de janeiro é ${investimentoMensal[0]}`);
+console.log(`O valor investido no mês de fevereiro é ${investimentoMensal[1]}`);
+console.log(`O valor investido no mês de março é ${investimentoMensal[2]}`);
+console.log(`O valor investido no mês de abril é ${investimentoMensal[3]}`);
+console.log(`O valor investido no mês de maio é ${investimentoMensal[4]}`);
+console.log(`O valor investido no mês de junho é ${investimentoMensal[5]}`);
+console.log(`O valor investido no mês de julho é ${investimentoMensal[6]}`);
+
