ce97927c58a67bf16e73292c6a4d7ad418788c11
FellypeGC <gabrielfellypesteve@gmail.com>
2023-08-21 00:41:48 -0300

    Create tiposReferencia.js

---
+++ modulo2/aula14/tiposReferencia.js
@@ -0,0 +1,22 @@
+let musica = {
+    nome: 'Let it Be',
+    artista: 'Beatles',
+    album: 'Let it Be',
+};
+
+let listaDeBandas = ['Beatles',
+    'Rolling Stones',
+    'Led Zappelin',
+    'Cássia Eller',
+    'Chico Buarque',
+];
+
+// console.log(listaDeBandas[4]);
+// listaDeBandas[5] = 'Skank';
+// listaDeBandas[3] = 'Deep Purple';
+// console.log(listaDeBandas[5]);
+// console.log(listaDeBandas);
+
+musica.album = 'Past Masters';
+musica.ano = 1969;
+console.log(musica);
