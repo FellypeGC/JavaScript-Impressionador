e1485706cccbae82424df98c6a3f307c5a545d89
FellypeGC <gabrielfellypesteve@gmail.com>
2023-09-01 20:07:56 -0300

    Create script.js

---
+++ modulo2/ex pt2/script.js
@@ -0,0 +1,11 @@
+console.log('O resultado do exercício 1:');
+
+const listaNumber = [1, 2, 3, 4, 5];
+
+console.log(listaNumber);
+
+console.log(`O valor do elemento 0 da minha lista é ${listaNumber[0]}`);
+console.log(`O valor do elemento 1 da minha lista é ${listaNumber[1]}`);
+console.log(`O valor do elemento 2 da minha lista é ${listaNumber[2]}`);
+console.log(`O valor do elemento 3 da minha lista é ${listaNumber[3]}`);
+console.log(`O valor do elemento 4 da minha lista é ${listaNumber[4]}`);
