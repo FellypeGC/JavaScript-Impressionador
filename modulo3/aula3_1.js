fcd72f0ed01a4e903101113377be5bf1f317a93c
FellypeGC <gabrielfellypesteve@gmail.com>
2023-09-03 01:58:26 -0300

    Create aula3_1.js

---
+++ modulo3/aula3_1.js
@@ -0,0 +1,16 @@
+// console.log('Olá, boa noite!');
+// console.log('Pegue aqui a sua comanda!');
+// console.log('Bom apetite!!');
+// console.log('---------------------------');
+
+function pegarComanda() {
+    console.log('Olá, boa noite!');
+    console.log('Pegue aqui a sua comanda!');
+    console.log('Bom apetite!!');
+    console.log('---------------------------');
+}
+
+pegarComanda();
+pegarComanda();
+pegarComanda();
+pegarComanda();
