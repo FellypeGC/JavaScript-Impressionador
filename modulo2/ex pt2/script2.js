6b19dac5d35aca533636b5ad2c86cea555e878ea
FellypeGC <gabrielfellypesteve@gmail.com>
2023-09-03 15:04:50 -0300

    Create script2.js

---
+++ modulo2/ex pt2/script2.js
@@ -0,0 +1,16 @@
+const meuComputador = {
+    Gabinete: 'Mancer Ward',
+    PlacaMãe: 'Asrock B450m Steel Legend',
+    MemóriaRAM: 'HyperX Kingston Fury 2x8GB 3200Mhz',
+    Processador: 'AMD Ryzen 5 5600G',
+    WaterCooler: 'Rise Mode 240mm RM-WCB-02-RGB',
+    GPU: 'NVIDIA RTX 3060 (em breve)'
+};
+
+console.log('---------ESPECIFICAÇÔES DO PC---------');
+console.log(`Gabinete: ${meuComputador.Gabinete}`);
+console.log(`Placa Mãe: ${meuComputador.PlacaMãe}`);
+console.log(`Memória RAM: ${meuComputador.MemóriaRAM}`);
+console.log(`Processador: ${meuComputador.Processador}`);
+console.log(`Water Cooler: ${meuComputador.WaterCooler}`);
+console.log(`Placa de Vídeo: ${meuComputador.GPU}`);
