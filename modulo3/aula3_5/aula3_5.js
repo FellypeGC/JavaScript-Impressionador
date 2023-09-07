efe91baf81b6100b7209dae851d53be4ae4eb73d
FellypeGC <gabrielfellypesteve@gmail.com>
2023-09-07 17:04:29 -0300

    Create aula3_5.js

---
+++ modulo3/aula3_5/aula3_5.js
@@ -0,0 +1,14 @@
+const primeiraVariavelDoCodigo = 'texto inicial';
+
+function printToConsole() {
+    const primeiraVariavelDoCodigo = 'segundo texto';
+    function secondFunction(){
+        const primeiraVariavelDoCodigo = 'terceiro texto';
+        console.log(primeiraVariavelDoCodigo);
+    }
+    console.log(primeiraVariavelDoCodigo);
+    secondFunction();
+}
+
+printToConsole();
+// console.log(primeiraVariavelDoCodigo);
