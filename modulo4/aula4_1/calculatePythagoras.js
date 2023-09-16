8be2f3d5ba89bff071ce62c0182791af575b3590
FellypeGC <gabrielfellypesteve@gmail.com>
2023-09-16 19:16:10 -0300

    Update calculatePythagoras.js

--- modulo4/aula4_1/calculatePythagoras.js
+++ modulo4/aula4_1/calculatePythagoras.js
@@ -1,3 +1,11 @@
 function calculateSquareRoot(radicand) {
     return radicand ** (1 / 2);
 }
+
+function calculatePythagoras(side1, side2) {
+    const sum = side1 ** 2 + side2 ** 2
+    return calculateSquareRoot(sum); 
+}
+
+console.log(`O tamanho da hipotenusa de um triangulo de lados 3 e 4 é ${calculatePythagoras(3, 4)}`);
+
