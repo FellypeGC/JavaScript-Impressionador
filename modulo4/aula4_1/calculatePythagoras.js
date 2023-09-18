90ba3458d2cb0e9d3852ff04ef70b6f3e7e1db3b
FellypeGC <gabrielfellypesteve@gmail.com>
2023-09-18 00:35:37 -0300

    Update calculatePythagoras.js

--- modulo4/aula4_1/calculatePythagoras.js
+++ modulo4/aula4_1/calculatePythagoras.js
@@ -1,10 +1,19 @@
 function calculateSquareRoot(radicand) {
+    console.log('O valor do meu radicando é: ', radicand);
     return radicand ** (1 / 2);
 }
 
 function calculatePythagoras(side1, side2) {
+    console.log("Os tamanhos dos catetos do meu triangulo retangulo são: ", 
+    side1, 
+    side2
+    );
+
     const sum = side1 ** 2 + side2 ** 2
+    console.log('O valor da variável sum é: ', sum);
+
     return calculateSquareRoot(sum); 
+    
 }
 
 console.log(`O tamanho da hipotenusa de um triangulo de lados 3 e 4 é ${calculatePythagoras(3, 4)}`);
