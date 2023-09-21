05916d7622f4967aefa3d32b4dce33620ce0c71c
FellypeGC <gabrielfellypesteve@gmail.com>
2023-09-20 23:57:18 -0300

    Update exampleCode.js

--- modulo5/aula5_1/exampleCode.js
+++ modulo5/aula5_1/exampleCode.js
@@ -1,8 +1,14 @@
+const usuario = { nome: 'Daniel', idade: 29, time: 'Fluminense' };
 const mensagemDeBoasVindas1 = 'Bem vindo, torcedor Tricolor!';
 const mensagemDeBoasVindas2 = 'Bem vindo, torcedor Rubro-Negro!';
 
-console.log(mensagemDeBoasVindas1);
-console.log(mensagemDeBoasVindas2);
+if (usuario.time === 'Fluminense') {
+    console.log(mensagemDeBoasVindas1);
+} 
+if (usuario.time === 'Flamengo') {
+    console.log(mensagemDeBoasVindas2);
+}
+
 
 const mensagemDeEscolha = 'Clique no setor para qual deseja comprar seu ingresso!';
 const mensagemFinal = 'Divirta-se no Maracanã!!!';
