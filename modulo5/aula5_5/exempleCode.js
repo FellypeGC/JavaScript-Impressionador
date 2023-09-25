503ed6262d6c829c572d0116eb719e128586f46a
FellypeGC <gabrielfellypesteve@gmail.com>
2023-09-25 18:04:36 -0300

    Create exempleCode.js

---
+++ modulo5/aula5_5/exempleCode.js
@@ -0,0 +1,19 @@
+const usuario = { nome: 'Daniel', idade: 29, time: 'Fluminense' };
+const mensagemDeBoasVindas1 = 'Bem vindo, torcedor Tricolor!';
+const mensagemDeBoasVindas2 = 'Bem vindo, torcedor Rubro-Negro!';
+const mensagemDeBoasVindasGeral = 'Bem vindo, Amante do Esporte';
+
+
+usuario.time === 'Fluminense' && usuario.idade >= 18
+    ? console.log(mensagemDeBoasVindas1) 
+    : usuario.time === 'Flamengo' && usuario.idade >= 18
+    ? console.log(mensagemDeBoasVindas2)
+    : usuario.idade >= 18 
+    ? console.log(mensagemDeBoasVindasGeral) 
+    : console.log('Não vendemos ingresso para torcedores menores de 18 anos');
+
+const mensagemDeEscolha = 'Clique no setor para qual deseja comprar seu ingresso!';
+const mensagemFinal = 'Divirta-se no Maracanã!!!';
+
+console.log(mensagemDeEscolha);
+console.log(mensagemFinal);
