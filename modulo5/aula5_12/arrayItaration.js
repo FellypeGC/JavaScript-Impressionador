f03d502edf595421c1f1ea76d414acfc7aecfde2
FellypeGC <gabrielfellypesteve@gmail.com>
2023-09-28 18:15:02 -0300

    Create arrayItaration.js

---
+++ modulo5/aula5_12/arrayItaration.js
@@ -0,0 +1,27 @@
+const names = [
+    'Daniel',
+    'Izabelle',
+    'Fabrício',
+    'Rodrigo',
+    'Jorge',
+    'Lira',
+    'Camila',
+    'Matheus',
+    'Gabriel',
+];
+
+const namesObject = {
+    0 :'Daniel',
+    1: 'Izabelle',
+    2: 'Fabrício',
+    3: 'Rodrigo',
+    4: 'Jorge',
+    5: 'Lira',
+    6: 'Camila',
+    7: 'Matheus',
+    8: 'Gabriel',
+}
+
+for (personName of names) {
+    console.log(personName);
+}
