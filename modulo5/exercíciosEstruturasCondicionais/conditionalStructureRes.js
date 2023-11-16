6eb7746cdc0604b8415de867da6a53d50891a729
FellypeGC <gabrielfellypesteve@gmail.com>
2023-11-15 23:04:45 -0300

    Update conditionalStructureRes.js

--- modulo5/exercíciosEstruturasCondicionais/conditionalStructureRes.js
+++ modulo5/exercíciosEstruturasCondicionais/conditionalStructureRes.js
@@ -2,22 +2,22 @@
 console.log('Resolução Exercício 1');
 
 function verificaDesconto(valorDaCompra) {
-    if (valorDaCompra >= 100) {
-        console.log(
-            'Parabéns! Você tem direito ao desconto de 10%'
-        );
-        const valorComDesconto = valorDaCompra * 0.9;
-        console.log(
-            `Valor com desconto: R$ ${valorComDesconto}`
-        )
-    } else {
-        console.log(
-            'Infelizmente você não tem direito ao desconto de 10%. Somente compras de R$100,00 ou mais.'
-        );
-        console.log(
-            `Valor da compra: R$ ${valorDaCompra}`
-        );
-    }
+  if (valorDaCompra >= 100) {
+      console.log(
+          'Parabéns! Você tem direito ao desconto de 10%'
+      );
+      const valorComDesconto = valorDaCompra * 0.9;
+      console.log(
+          `Valor com desconto: R$ ${valorComDesconto}`
+      )
+  } else {
+      console.log(
+          'Infelizmente você não tem direito ao desconto de 10%. Somente compras de R$100,00 ou mais.'
+      );
+      console.log(
+          `Valor da compra: R$ ${valorDaCompra}`
+      );
+  }
 }
 
 const valorDaCompra = 80;
@@ -30,11 +30,11 @@ verificaDesconto(valorDaCompra2);
 console.log('Resolução Exercício 2');
 
 function verificarEstoque(quantidadeDisponivel) {
-    if (quantidadeDisponivel > 0) {
-        return 'Produto disponível.';
-    } else {
-        return 'Produto esgotado';
-    }
+  if (quantidadeDisponivel > 0) {
+      return 'Produto disponível.';
+  } else {
+      return 'Produto esgotado';
+  }
 }
 
 const quantidadeExemplo = 5;
@@ -47,13 +47,13 @@ console.log(verificarEstoque(quantidadeExemplo2));
 console.log('Resolução Exercício 3');
 
 function categoriaCliente(pontuaçãoDeFidelidade) {
-    if (pontuaçãoDeFidelidade >= 85) {
-        return 'Cliente Premium';
-    } else if (pontuaçãoDeFidelidade >= 50) {
-        return 'Cliente Regular';
-    } else {
-        return 'Cliente Comum';
-    }
+  if (pontuaçãoDeFidelidade >= 85) {
+      return 'Cliente Premium';
+  } else if (pontuaçãoDeFidelidade >= 50) {
+      return 'Cliente Regular';
+  } else {
+      return 'Cliente Comum';
+  }
 }
 
 const pontuaçãoCliente1 = 86; 
@@ -68,19 +68,19 @@ console.log(categoriaCliente(pontuaçãoCliente3));
 console.log('Resolução Exercício 4');
 
 function desempenhoIndividualDeVendas(vendasMensais, metaDeVendas) {
-    const percentualAlcancado = (vendasMensais / metaDeVendas) * 100;
+  const percentualAlcancado = (vendasMensais / metaDeVendas) * 100;
 
-    if (percentualAlcancado >= 100) {
-        return 'Excelente Desempenho';
-    } else if (percentualAlcancado >= 90 && percentualAlcancado < 100) {
-        return 'Muito Bom Desempenho';
-    } else if (percentualAlcancado >= 80 && percentualAlcancado < 90) {
-        return 'Bom Desempenho';
-    } else if (percentualAlcancado >= 61 && percentualAlcancado < 80) {
-        return 'Desempenho Satisfatório';
-    } else {
-        return 'Desempenho Insatisfatório';
-    }
+  if (percentualAlcancado >= 100) {
+      return 'Excelente Desempenho';
+  } else if (percentualAlcancado >= 90 && percentualAlcancado < 100) {
+      return 'Muito Bom Desempenho';
+  } else if (percentualAlcancado >= 80 && percentualAlcancado < 90) {
+      return 'Bom Desempenho';
+  } else if (percentualAlcancado >= 61 && percentualAlcancado < 80) {
+      return 'Desempenho Satisfatório';
+  } else {
+      return 'Desempenho Insatisfatório';
+  }
 }
 
 const vendasMensais = 7500;
@@ -98,11 +98,11 @@ console.log(`Meta de Vendas: R$ ${metaDeVendas}`);
 console.log('Resolução Exercício 5');
 
 function verificarVelocidade(velocidadeDeUmVeículo) {
-    if (velocidadeDeUmVeículo <= 80) {
-        return true;
-    } else {
-        return false; 
-    }
+  if (velocidadeDeUmVeículo <= 80) {
+      return true;
+  } else {
+      return false; 
+  }
 }
 
 const carro1 = 50;
@@ -115,15 +115,15 @@ console.log(verificarVelocidade(carro2));
 console.log('Resolução Exercício 6');
 
 function mensagemSemaforo(corSemaforo) {
-    const mensagem = 
-    corSemaforo === 'vermelho'
-    ? 'Pare o veículo'
-    :corSemaforo === 'amarelo'
-    ? 'Reduza a velocidade e pare se seguro.'
-    : corSemaforo === 'verde'
-    ? 'Você pode continuar com cuidado.'
-    : 'Cor do Semáforo desconhecida. Siga com atenção';
-    return mensagem;
+  const mensagem = 
+  corSemaforo === 'vermelho'
+  ? 'Pare o veículo'
+  :corSemaforo === 'amarelo'
+  ? 'Reduza a velocidade e pare se seguro.'
+  : corSemaforo === 'verde'
+  ? 'Você pode continuar com cuidado.'
+  : 'Cor do Semáforo desconhecida. Siga com atenção';
+  return mensagem;
 }
 
 const corSemaforoExemplo = 'verde';
@@ -140,22 +140,22 @@ console.log(mensagem3);
 console.log('Resolução Exercício 7');
 
 function coresDoSemaforo(corSemaforoEx) {
-    let mensagem;
-    switch (corSemaforoEx) {
-    case "vermelho":
-    mensagem = "Pare o veículo imediatamente!";
-    break;
-    case "amarelo":
-    mensagem = "Reduza a velocidade e pare se seguro.";
-    break;
-    case "verde":
-    mensagem = "Você pode continuar com cuidado.";
-    break;
-    default:
-    mensagem = "Cor do semáforo desconhecida. Siga com atenção.";
-    }
-    return mensagem;
-   }
+  let mensagem;
+  switch (corSemaforoEx) {
+  case "vermelho":
+  mensagem = "Pare o veículo imediatamente!";
+  break;
+  case "amarelo":
+  mensagem = "Reduza a velocidade e pare se seguro.";
+  break;
+  case "verde":
+  mensagem = "Você pode continuar com cuidado.";
+  break;
+  default:
+  mensagem = "Cor do semáforo desconhecida. Siga com atenção.";
+  }
+  return mensagem;
+  }
   
 const corSemaforoExemploEx = "verde"; 
 console.log(coresDoSemaforo(corSemaforoExemplo));
@@ -164,33 +164,33 @@ console.log(coresDoSemaforo(corSemaforoExemplo));
 console.log('Resolução Exercício 8');
 
 function verificarPlaca(ultimoDigitoPlaca) {
-    let diaRodizio;
-    switch (ultimoDigitoPlaca) {
-    case 1:
-    case 2:
-    diaRodizio = "Segunda-feira";
-    break;
-    case 3:
-    case 4:
-    diaRodizio = "Terça-feira";
-    break;
-    case 5:
-    case 6:
-    diaRodizio = "Quarta-feira";
-    break;
-    case 7:
-    case 8:
-    diaRodizio = "Quinta-feira";
-    break;
-    case 9:
-    case 0:
-    diaRodizio = "Sexta-feira";
-    break;
-    default:
-    diaRodizio = "Placa inválida";
-    }
-    return `O dia de rodízio é na ${diaRodizio}.`;
-   }
+  let diaRodizio;
+  switch (ultimoDigitoPlaca) {
+  case 1:
+  case 2:
+  diaRodizio = "Segunda-feira";
+  break;
+  case 3:
+  case 4:
+  diaRodizio = "Terça-feira";
+  break;
+  case 5:
+  case 6:
+  diaRodizio = "Quarta-feira";
+  break;
+  case 7:
+  case 8:
+  diaRodizio = "Quinta-feira";
+  break;
+  case 9:
+  case 0:
+  diaRodizio = "Sexta-feira";
+  break;
+  default:
+  diaRodizio = "Placa inválida";
+  }
+  return `O dia de rodízio é na ${diaRodizio}.`;
+  }
    
-   const ultimoDigitoPlacaExemplo = 3; 
-   console.log(verificarPlaca(ultimoDigitoPlacaExemplo));
+const ultimoDigitoPlacaExemplo = 3; 
+console.log(verificarPlaca(ultimoDigitoPlacaExemplo));
