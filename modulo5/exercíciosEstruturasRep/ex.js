7a31bff4cba09595d0a17ef8d59e353e722b230d
FellypeGC <gabrielfellypesteve@gmail.com>
2023-11-15 23:26:32 -0300

    ex02

--- modulo5/exercíciosEstruturasRep/ex.js
+++ modulo5/exercíciosEstruturasRep/ex.js
@@ -1,5 +1,34 @@
 /* Exercício 1: Cálculo do Faturamento Semanal */
 
-for (let i = 0; 0 < 2; i++) {
-  console.log(i)
+const vendasPorDia = [20, 15, 25, 18, 22, 30, 28];
+const precoPorHamburguer = 10;
+
+function calcularFaturamentoSemanal(vendasPorDia, precoPorHamburguer) {
+  let faturamentoSemanal = 0;
+  for (let index = 0; index < vendasPorDia.length; index++) {
+    faturamentoSemanal += vendasPorDia[index] * precoPorHamburguer;
+  }
+  return faturamentoSemanal;
 }
+
+const faturamentoTotal = calcularFaturamentoSemanal(vendasPorDia, precoPorHamburguer);
+
+console.log(`O faturamento total da semana é R$${faturamentoTotal}`);
+
+/* Exercício 2: Listagem do Cardápio Digital */
+
+const cardapio = [
+  {nome: "Hambúrguer", preco: 10},
+  {nome: "Batata frita", preco: 5},
+  {nome: "Refrigerante", preco: 3},
+  {nome: "Milkshake", preco: 7},
+];
+
+function exibirCardapio(cardapio) {
+  console.log('Cardápio da Lanchonete: ');
+  for (let contador = 0; contador < cardapio.length; contador++) {
+    console.log(`${cardapio[contador].nome}: ${cardapio[contador].preco}`)
+  }
+}
+
+exibirCardapio(cardapio);
--- modulo5/exercíciosEstruturasRep/node_fd1227c31d889.tmp.js
+++
@@ -1,5 +0,0 @@
-/* Exercício 1: Cálculo do Faturamento Semanal */
-
-for (let i = 0; 0 < 20; i++) {
-  console.log(i)
-}
