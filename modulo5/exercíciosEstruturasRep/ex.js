90a4e2404a6be4251ca663ae631f2e1dd94ec41c
FellypeGC <gabrielfellypesteve@gmail.com>
2023-11-15 23:09:04 -0300

    ex1 estrutura de repetição

---
+++ modulo5/exercíciosEstruturasRep/ex.js
@@ -0,0 +1,5 @@
+/* Exercício 1: Cálculo do Faturamento Semanal */
+
+for (let i = 0; 0 < 2; i++) {
+  console.log(i)
+}
---
+++ modulo5/exercíciosEstruturasRep/node_fd1227c31d889.tmp.js
@@ -0,0 +1,5 @@
+/* Exercício 1: Cálculo do Faturamento Semanal */
+
+for (let i = 0; 0 < 20; i++) {
+  console.log(i)
+}
