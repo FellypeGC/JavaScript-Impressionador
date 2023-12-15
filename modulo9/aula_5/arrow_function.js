d8b50ad3ef4bafcf390a0d40b4de0217ce1c43e1
FellypeGC <gabrielfellypesteve@gmail.com>
2023-12-15 16:56:39 -0300

    Update arrow_function.js

--- modulo9/aula_5/arrow_function.js
+++ modulo9/aula_5/arrow_function.js
@@ -21,3 +21,10 @@ const variavelQueAbrigaArrowFunction = () => {
 };
 
 variavelQueAbrigaArrowFunction();
+
+const soma = (n1, n2) => {
+  return n1 + n2;
+} 
+
+const resultado = soma(4, 5);
+console.log(`O resultado entre a soma dos números 4 e 5 é: ${resultado}`);
