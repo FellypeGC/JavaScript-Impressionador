1643401182354a008ce1ec7a3d25953fe5aeafb2
FellypeGC <gabrielfellypesteve@gmail.com>
2023-12-16 01:41:24 -0300

    Create differentArrow.js

---
+++ modulo9/aula_6/differentArrow.js
@@ -0,0 +1,23 @@
+const myLoggingFunction = (message) => {
+  console.log(message);
+  console.log('Segunda instrução da minha arrow function');
+};
+
+// const myNormalLoggingFunction = function (message) {
+//   console.log(message);
+// };
+
+// myLoggingFunction('Teste da primeira arrow function do arquivo');
+
+// myNormalLoggingFunction('Teste da minha função normal armazenada numa variável');
+
+// const myLoggingFunctionWithOnlyOneParameter = message => {
+//   console.log(message);
+// }
+
+const myLoggingFunctionWithOnlyOneInstruction = (message, message2) => console.log(message + message2);
+
+
+// myLoggingFunctionWithOnlyOneParameter('chamada de função da minha arrow function sem parênteses');
+
+myLoggingFunctionWithOnlyOneInstruction('Teste da função: ', 'bora ver se deu certo');
