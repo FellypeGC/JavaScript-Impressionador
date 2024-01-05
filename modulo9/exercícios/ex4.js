c3ff29fd822f840883d9bd77c1123ad6598fa8b3
FellypeGC <gabrielfellypesteve@gmail.com>
2024-01-05 20:03:16 -0300

    Update ex4.js

--- modulo9/exercícios/ex4.js
+++ modulo9/exercícios/ex4.js
@@ -1,3 +1,20 @@
 /* Exercício 4: Encadeamento de Promises
-Crie duas Promises: uma que seja resolvida para "Olá" após um atraso de 1 segundo e outra que seja resolvida para "Mundo" após um atraso de 2 segundos. Encadeie essas Promises usando .then() para imprimir
-"Olá, Mundo" no console. */
+Crie duas Promises: uma que seja resolvida para "Olá" após um atraso de 1 segundo e outra que seja resolvida para "Mundo" após um atraso de 2 segundos. Encadeie essas Promises usando .then() para imprimir "Olá, Mundo" no console. */
+
+async function promises() {
+   const promise1 = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve('Olá,');
+      }, 1000);
+    }).then(console.log);
+
+  const promise2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      setTimeout(() => {
+        resolve('Mundo');
+      }, 2000);
+    })
+  }).then(console.log);
+}
+
+promises();
