e7bbfd3bac5a6b66af91855c487e829d88e802a5
FellypeGC <gabrielfellypesteve@gmail.com>
2024-01-05 17:54:27 -0300

    Update ex3.js

--- modulo9/exercícios/ex3.js
+++ modulo9/exercícios/ex3.js
@@ -1,3 +1,9 @@
 /* Exercício 3: Conceitos Básicos de Promises
 Crie uma nova Promise que seja resolvida após um atraso de 1 segundo e retorne a string "Promise
 resolvida". Use .then() para lidar com o valor resolvido e imprimir no console. */
+
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('Promise resolvida');
+  }, 1000);
+}).then(console.log);
