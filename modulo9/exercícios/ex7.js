234c2e4bc6b171a34028d71204835f44b1434eec
FellypeGC <gabrielfellypesteve@gmail.com>
2024-01-07 14:26:38 -0300

    Update ex7.js

--- modulo9/exercícios/ex7.js
+++ modulo9/exercícios/ex7.js
@@ -3,5 +3,18 @@ Escreva uma função assíncrona buscarDados que usa await para simular a busca 
 um atraso de 2 segundos. Imprima os dados buscados no console. */
 
 async function fetchData() {
-  
-}
+  console.log('Buscando dados da API');
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve()
+    }, 2000)
+  })
+
+  const dataAPI = {
+    data: 'Dados da API',
+  };
+
+  console.log(`Dados encontrados: ${dataAPI.data}`);
+};
+
+fetchData();
