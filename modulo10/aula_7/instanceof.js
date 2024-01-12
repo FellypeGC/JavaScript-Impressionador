8cda1f0e5463a0137b0c4f1181c804a7b4f2828c
FellypeGC <99624115+FellypeGC@users.noreply.github.com>
2024-01-12 13:23:56 -0300

    Create instanceof.js

---
+++ modulo10/aula_7/instanceof.js
@@ -0,0 +1,22 @@
+function BankAccount(personName, initialBalance, address) {
+  this.personName = personName;
+  this.balance = initialBalance;
+  this.address = address;
+
+  this.printInfo = function () {
+    console.log(
+      `Essa conta pertence a ${
+        this.personName
+      }, residente do endereço '${
+        this.address
+      }' e possuí saldo de ${
+        Number(this.balance).toFixed(2)
+      } reais.`
+    );
+  };
+}
+
+const ba1 = new BankAccount('Daniel', 2000, 'Rua que sobe e desce');
+
+ba1.printInfo();
+console.log({} instanceof BankAccount);
