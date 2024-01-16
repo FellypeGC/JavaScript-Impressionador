69a2fa4687cd98f68a4b5e94ac9e07e268884826
FellypeGC <99624115+FellypeGC@users.noreply.github.com>
2024-01-16 11:52:24 -0300

    Update Stopwatch.js

--- modulo10/stopwatch/Stopwatch.js
+++ modulo10/stopwatch/Stopwatch.js
@@ -1,11 +1,11 @@
-class Stopwacth {
+class Stopwatch {
   elapsedTimeInSeconds = 0;
   intervalId = null;
 
   start() {
     this.intervalId = setInterval(() => {
       this.elapsedTimeInSeconds++;
-      console.log(this.formatTime(this.elapsedTimeInSeconds));
+      console.log(Stopwatch.formatTime(this.elapsedTimeInSeconds));
     }, 1000);
   }
 
@@ -19,12 +19,26 @@ class Stopwacth {
 
   static formatTime(timeInSeconds) {
     const hours = Math.floor(timeInSeconds / 3600);
-    const minutes = Math.floor(timeInSeconds % 3600) / 60;
+    const minutes = Math.floor((timeInSeconds % 3600) / 60);
     const seconds = timeInSeconds - hours * 3600 - minutes * 60;
 
-    return `${hours}:${minutes}:${seconds}`;
+    return `${Stopwatch.zeroPadding(hours)}:${Stopwatch.zeroPadding(minutes)}:${Stopwatch.zeroPadding(seconds)}`;
+  }
+
+  static zeroPadding(originalNumber, desiredAmountDigits = 2) {
+    let stringNumber = String(originalNumber);
+    const zeroesRequired = desiredAmountDigits - stringNumber.length;
+    if (zeroesRequired <= 0) {
+      return stringNumber;
+    }
+
+    for(let counter = 0; counter < zeroesRequired; counter++) {
+      stringNumber = `0${stringNumber}`;
+    }
+
+    return stringNumber;
   }
 }
 
-const sw1 = new Stopwacth();
+const sw1 = new Stopwatch();
 sw1.start();
