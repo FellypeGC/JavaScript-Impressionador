a00240d2a51ba1d96cee9a16f913e10bde307b21
FellypeGC <99624115+FellypeGC@users.noreply.github.com>
2024-02-09 22:38:07 -0300

    Update index.js

--- modulo11/aula_1/index.js
+++ modulo11/aula_1/index.js
@@ -1,27 +1,51 @@
-function writeMessageOnConsoleAndReturnTwo(message) {
-  console.log(message);
-  return 2;
-}
+// function writeMessageOnConsoleAndReturnTwo(message) {
+//   console.log(message);
+//   return 2;
+// }
 
-//palavra-chave function <= 
+//palavra-chave function X
 //nome da função X
 //parênteses - parâmetros <=
 //bloco de chaves com instruções dentro <=
 //instrução de retorno V
 
-const variableThatStoresFunction = function (message){
+// const variableThatStoresFunction = function (message){
+//   console.log(message);
+//   return undefined;
+// };
+
+// const variableThatStoresFunction2 = function (message) {
+//   console.log(message);
+// };
+
+// console.log(
+//   `O retorno da função na primeira variável é: ${variableThatStoresFunction('função da primeira variável')}`
+// );
+
+// console.log(
+//   `O retorno da função na segunda variável é: ${variableThatStoresFunction2('função da segundaa variável')}`
+// );
+
+const writeMessageOnConsoleAndReturnTwo = (message) => {
   console.log(message);
-  return undefined;
+  return 2;
+}
+
+function squareNumber(originalNumber) {
+  return originalNumber * originalNumber;
+}
+
+const arrowFunctionQueRetornaObjeto = () => {
+  return {
+    id: 0,
+    nome: 'Daniel Porto',
+  };
 };
 
-const variableThatStoresFunction2 = function (message) {
-  console.log(message);
-};
+const squareNumberArrow = originalNumber => originalNumber * originalNumber;
 
-console.log(
-  `O retorno da função na primeira variável é: ${variableThatStoresFunction('função da primeira variável')}`
-);
 
-console.log(
-  `O retorno da função na segunda variável é: ${variableThatStoresFunction2('função da segundaa variável')}`
-);
+// console.log(`2 ao quadrado vale ${squareNumber(2)}`);
+// console.log(`3 ao quadrado vale ${squareNumber(3)}`);
+
+console.log(arrowFunctionQueRetornaObjeto());
