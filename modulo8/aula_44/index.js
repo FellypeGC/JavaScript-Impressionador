3c959a394f4cd19c184d07d45048794864e4826b
FellypeGC <gabrielfellypesteve@gmail.com>
2024-03-25 17:46:15 -0300

    c

---
+++ modulo8/aula_44/index.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const formulario = document.getElementById('meuFormulario');
+
+  formulario.addEventListener('submit', function (event) {
+    if (!validarCamposObrigatorios()) {
+      event.preventDefault(); // Impede a submissão do formulário se houver erros
+    }
+
+    function validarCamposObrigatorios() {
+      let camposObrigatorios = document.querySelectorAll('.obrigatorio');
+      let camposValidos = true;
+
+      for (let i = 0; camposObrigatorios.length > i; i++) {
+        let campo = camposObrigatorios[i];
+
+        if (campo.value === '' || campo.value === null) {
+          console.log('campo obrigatorio');
+          camposValidos = false;
+        }
+      } // elemento 0, 1, 2, 3
+      return camposValidos;
+    }
+  });
+
+})
