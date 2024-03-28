55668434c0e8e59bd5696ddee2706821ae293859
FellypeGC <gabrielfellypesteve@gmail.com>
2024-03-27 22:58:06 -0300

    Update index.js

--- modulo8/aula_44, 45, 46, 47, 48, 49, 50/index.js
+++ modulo8/aula_44, 45, 46, 47, 48, 49, 50/index.js
@@ -4,24 +4,37 @@ document.addEventListener('DOMContentLoaded', function () {
   formulario.addEventListener('submit', function (event) {
     if (!validarCamposObrigatorios()) {
       event.preventDefault(); // Impede a submissão do formulário se houver erros
-      console.log(!validarCamposObrigatorios())
     }
 
-    function validarCamposObrigatorios() {
-      let camposObrigatorios = document.querySelectorAll('.obrigatorio');
-      let camposValidos = true;
-
-      for (let i = 0; camposObrigatorios.length > i; i++) {
-        let campo = camposObrigatorios[i];
-
-        if (campo.value === '' || campo.value === null) {
-          console.log('campo obrigatorio');
-          camposValidos = false;
-        }
-      } // elemento 0, 1, 2, 3
-      console.log(camposValidos)
-      return camposValidos;
+    if (!compararValores()) {
+      event.preventDefault();
     }
   });
 
-})
+  function validarCamposObrigatorios() {
+    let camposObrigatorios = document.querySelectorAll('.obrigatorio');
+    let camposValidos = true;
+
+    for (let i = 0; camposObrigatorios.length > i; i++) {
+      let campo = camposObrigatorios[i];
+
+      if (campo.value === '' || campo.value === null) {
+        console.log('campo obrigatorio');
+        camposValidos = false;
+      }
+    } // elemento 0, 1, 2, 3
+    console.log(camposValidos)
+    return camposValidos;
+  }
+
+  function compararValores() {
+    const senha = document.getElementById('senha');
+    const confirmarSenha = document.getElementById('confirmarSenha');
+
+    if (senha.value !== confirmarSenha.value) {
+      console.log('As senhas não coincidem.');
+      return false;
+    }
+    return true;
+  }
+});
