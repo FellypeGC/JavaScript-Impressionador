44a5a0b4f022daefcc52626b434a43ad978fc246
FellypeGC <gabrielfellypesteve@gmail.com>
2024-05-13 20:26:42 -0300

    Refazendo o formulário de investimento

--- modulo19/calculadoraInvestimentos/src/investmentGoals.js
+++ modulo19/calculadoraInvestimentos/src/investmentGoals.js
@@ -2,7 +2,7 @@ function convertToMonthlyReturnRate(yearlyReturnRate) {
   return yearlyReturnRate ** (1/12);
 }
 
-function generateReturnsArray(
+export function generateReturnsArray(
   startingAmount = 0,
   timeHorizon = 0,
   timePeriod = 'monthly',
@@ -28,7 +28,7 @@ function generateReturnsArray(
 
   const returnsArray = [referenceInvestmentObject];
   for (let timeReference = 1; timeReference <= finalTimeHorizon; timeReference++) {
-    const totalAmount = (returnsArray[timeReference - 1].totalAmount * finalReturnRate) + monthlyContribution;
+    const totalAmount = returnsArray[timeReference - 1].totalAmount * finalReturnRate + monthlyContribution;
 
     const interestReturns = returnsArray[timeReference - 1].totalAmount * finalReturnRate;
 
@@ -44,6 +44,6 @@ function generateReturnsArray(
       totalAmount,
     });
   }
-
+  
   return returnsArray;
 }
