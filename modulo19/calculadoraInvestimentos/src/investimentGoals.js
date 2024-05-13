19ab416b04cd5d472fb352af880ff69f6e3e00a9
FellypeGC <gabrielfellypesteve@gmail.com>
2024-05-13 16:28:24 -0300

    Refazendo o formulário de investimento

--- modulo19/calculadoraInvestimentos/src/investimentGoals.js
+++ modulo19/calculadoraInvestimentos/src/investimentGoals.js
@@ -1,30 +1,24 @@
 function convertToMonthlyReturnRate(yearlyReturnRate) {
-  return yearlyReturnRate ** (1 / 12);
+  return yearlyReturnRate ** (1/12);
 }
 
-export function generateReturnsArray(
+function generateReturnsArray(
   startingAmount = 0,
   timeHorizon = 0,
-  timePeriod = "monthly",
+  timePeriod = 'monthly',
   monthlyContribution = 0,
   returnRate = 0,
-  returnTimeFrame = "monthly"
+  returnTimeFrame = 'monthly'
 ) {
   if (!timeHorizon || !startingAmount) {
-    throw new Error(
-      "Investimento inicial e prazo devem ser preenchidos com valores positivos."
-    );
+    throw new Error('Investimento inicial e prazo de investimento devem ser preenchidos com valores positivos.');
   }
 
-  const finalReturnRate =
-    returnTimeFrame === "monthly"
-      ? 1 + returnRate / 100
-      : convertToMonthlyReturnRate(1 + returnRate / 100);
+  const finalReturnRate = returnTimeFrame === 'monthly' ? 1 + returnRate /100 : convertToMonthlyReturnRate(1 + returnRate / 100);
 
-  const finalTimeHorizon =
-    timePeriod === "monthly" ? timeHorizon : timeHorizon * 12;
+  const finalTimeHorizon = timePeriod === 'monthly' ? timeHorizon : timeHorizon * 12;
 
-  const referenceInvestimentObject = {
+  const referenceInvestmentObject = {
     investedAmount: startingAmount,
     interestReturns: 0,
     totalInterestReturns: 0,
@@ -32,20 +26,20 @@ export function generateReturnsArray(
     totalAmount: startingAmount,
   };
 
-  const returnsArray = [referenceInvestimentObject];
-  for (
-    let timeReference = 1;
-    timeReference <= finalTimeHorizon;
-    timeReference++
-  ) {
-    const totalAmount = returnsArray[timeReference - 1].totalAmount * finalReturnRate + monthlyContribution;
+  const returnsArray = [referenceInvestmentObject];
+  for (let timeReference = 1; timeReference <= finalTimeHorizon; timeReference++) {
+    const totalAmount = (returnsArray[timeReference - 1].totalAmount * finalReturnRate) + monthlyContribution;
+
     const interestReturns = returnsArray[timeReference - 1].totalAmount * finalReturnRate;
+
     const investedAmount = startingAmount + monthlyContribution * timeReference;
+
     const totalInterestReturns = totalAmount - investedAmount;
+
     returnsArray.push({
       investedAmount,
       interestReturns,
-      totalAmount,
+      totalInterestReturns,
       month: timeReference,
       totalAmount,
     });
