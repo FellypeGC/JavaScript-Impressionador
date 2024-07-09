151c0f0752161c47994a9afe419d1338aef6e822
FellypeGC <gabrielfellypesteve@gmail.com>
2024-07-09 14:48:39 -0300

    modulo 20 add

---
+++ modulo21/aula_3/script.js
@@ -0,0 +1,14 @@
+export function addFiveNumbers(n1, n2, n3, n4, n5) {
+  return n1 + n2 + n3 + n4 + n5;
+}
+
+function selectBestContry() {
+  return 'Brasil';
+}
+
+global.console.log('Oiiiii');
+
+console.log(global.addFiveNumbers(1, 2, 3, 4, 5));
+
+console.log(module);
+
