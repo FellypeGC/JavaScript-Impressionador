afdc555c8262139cd9f1c695bfab3df150ae5179
FellypeGC <gabrielfellypesteve@gmail.com>
2024-07-11 17:42:05 -0300

    aula 2 e 3 modulo 22

---
+++ modulo22/aula_2/events.mjs
@@ -0,0 +1,13 @@
+import { EventEmitter } from "node:events";
+
+const eventEmitter = new EventEmitter();
+
+eventEmitter.on('Evento1 Lançado', 
+  () => console.log('Escutei o evento1!')
+);
+
+/*eventEmitter.addListener('Evento1 Lançado', 
+  () => console.log('Escutei o evento1!')
+);*/
+
+eventEmitter.emit('Evento1 Lançado');
