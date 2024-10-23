813cf47ed29e762234363460f644215e2dfb6d8a
FellypeGC <gabrielfellypesteve@gmail.com>
2024-10-23 14:24:36 -0300

    renomeando pastas

--- modulo20/aula_5/script.js
+++
@@ -1,8 +0,0 @@
-import { rename } from "node:fs/promises";
-
-const oldName = process.argv[2] //['node', './script.js']
-const newName = process.argv[3];
-
-rename(oldName, newName)
-  .then(() => console.log(`Arquivo renomeado para ${newName}`))
-  .catch(() => console.log(`Não foi possível renomear o arquivo ${oldName}`));
---
+++ modulo22/aula_5/script.js
@@ -0,0 +1,27 @@
+// import { selectBestContry as bestCountry } from "./script3.js";
+
+export function addFiveNumbers(n1, n2, n3, n4, n5) {
+  return n1 + n2 + n3 + n4 + n5;
+}
+
+function selectBestContry() {
+  return 'Brasil';
+}
+
+// global.console.log('Oiiiii');
+
+// console.log(global.addFiveNumbers(1, 2, 3, 4, 5));
+
+console.log(module.filename);
+
+const path = require('node:path');
+const pathObj = path.parse(module.filename);
+
+console.log(pathObj);
+
+// console.log(module);
+
+// const script3 = require('./script3');
+// console.log(script3.bestCountry());
+
+// console.log(selectBestContry());
