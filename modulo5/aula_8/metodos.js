16b85b687bdc803b2122b9d9156cffc8e94cb712
FellypeGC <gabrielfellypesteve@gmail.com>
2024-10-28 20:38:36 -0300

    aulas 8, 9 e 10 adicionadas.

---
+++ modulo5/aula_8/metodos.js
@@ -0,0 +1,13 @@
+let produtos = ['Carrinho', 'Boneca', 'Bola'];
+
+produtos.push('Navio'); // adicionar ao final da lista
+console.log(produtos);
+
+produtos.unshift('Video Game'); // adicionar no inicio da lista
+console.log(produtos);
+
+produtos.pop() // remover o último elemento
+console.log(produtos);
+
+produtos.shift(); // remove o primeiro elemento
+console.log(produtos);
