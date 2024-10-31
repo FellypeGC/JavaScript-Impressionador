ed02fc49575502c4f0cc04c28dd4c2234027479c
FellypeGC <gabrielfellypesteve@gmail.com>
2024-10-31 14:55:59 -0300

    Update exercicios.js

--- modulo5/aula_19, 20, 21/exercicios.js
+++ modulo5/aula_19, 20, 21/exercicios.js
@@ -17,11 +17,11 @@ const vendas = {
     return `Produto: ${this.produto}, 
     Quantidade: ${this.quantidade}, 
     Preço Unitário: ${this.precoUnitario}, 
-    Total: ${this.calcularTotal}`;
+    Total: ${this.calcularTotal()}`;
   }
 }
 
-// console.log(vendas.detalharVenda());
+console.log(vendas.detalharVenda());
 
 // Exercício 2: Adicione um método ao objeto Venda chamado aplicarDesconto que recebe
 //  umvalor percentual e atualiza o desconto da venda. Aplique um desconto de 10% e verifique o
@@ -31,8 +31,8 @@ vendas.aplicarDesconto = function (percentual) {
   this.desconto = percentual;
 }
 
-// vendas.aplicarDesconto(10);
-// console.log(`O desconto foi de: ${venda.desconto}, a Venda: ${venda.detalharVenda()}`);
+vendas.aplicarDesconto(10);
+console.log(`O desconto foi de: ${venda.desconto}, a Venda: ${venda.detalharVenda()}`);
 
 // Exercício 3: Adicione um método ao objeto Venda chamado atualizarQuantidade que
 // recebe um número e altera a quantidade do produto na venda. Atualize a quantidade e
@@ -42,9 +42,10 @@ vendas.atualizarQuantidade = function (novaQuantidade) {
   this.quantidade = novaQuantidade;
 }
 
-// vendas.atualizarQuantidade(3);
-// vendas.aplicarDesconto(20);
-// console.log(`A quantidade foi de: ${vendas.quantidade}, a Venda: ${vendas.detalharVenda()}`);
+vendas.atualizarQuantidade(3);
+vendas.aplicarDesconto(20);
+
+console.log(`A quantidade foi de: ${vendas.quantidade}, a Venda: ${vendas.detalharVenda()}`);
 
 // Exercício 4: Adicione um método chamado verificarEstoque que recebe a quantidade
 // disponível em estoque e retorna uma mensagem fixa indicando se a venda pode ser realizada
@@ -60,26 +61,66 @@ console.log(vendas);
 // Exercício 5: Retorne um array com todas as suas propriedades e valores do objeto Venda.
 // Exiba as propriedades e valores em um formato legível.
 
+const entradas = Object.entries(vendas);
+console.log(entradas);
+
 // Exercício 6: Obtenha um array com os nomes de todas as propriedades do objeto Venda.
 // Exiba as propriedades do objeto.
+
+const chaves = Object.keys(vendas);
+console.log(chaves);
 
 // Exercício 7: Obtenha um array com todos os valores das propriedades do objeto Venda.
 // Exiba os valores das propriedades.
 
+const values = Object.values(vendas);
+console.log(values);
+
 // Exercício 8: Verifique se o objeto Venda possui a propriedade desconto. Retorne a
 // verificação diretamente em uma mensagem fixa.
+
+function verificarPropriedadeDesconto() {
+  return 'Verificação concluída: ' + vendas.hasOwnProperty('desconto');
+}
+
+console.log(verificarPropriedadeDesconto());
 
 // Exercício 9: Crie um novo objeto NovaVenda que contenha as mesmas propriedades e
 // valores de Venda, mas com um produto e quantidade diferentes. Exiba o novo objeto
 // NovaVenda.
 
+const novaVenda = Object.assign({}, vendas, {
+  produto: 'Notebook',
+  quantidade: 1,
+});
+
+console.log(novaVenda);
+
 // Exercício 10: Adicione uma nova propriedade data ao objeto Venda, definindo-a como não
 //  enumerável. Exiba todas as propriedades do objeto Venda e, em seguida, exiba o valor da
 //  nova propriedade data separadamente.
+
+Object.defineProperty(vendas, 'data', {
+  value: '2024-08-03',
+  enumerable: false,
+});
+
+console.log("Data da venda " + vendas.data);
 
 //  Exercício 11: Crie um novo objeto DescontoEspecial que herde as propriedades do objeto
 //  Venda, mas com um desconto fixo de 20%. Exiba as propriedades do novo objeto e verifique
 //  se o desconto foi aplicado corretamente.
 
+const descontoEspecial = Object.create(vendas);
+descontoEspecial.desconto = 20;
+
+console.log(descontoEspecial.detalharVenda());
+console.log(vendas.detalharVenda());
+
 //  Exercício 12: Remova a propriedade desconto do objeto Venda. Verifique se a propriedade
 //  foi removida com sucesso e exiba as propriedades restantes do objeto.
+
+delete vendas.desconto;
+delete vendas.aplicarDesconto;
+
+console.log(Object.keys(vendas));
