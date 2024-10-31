116d6460dd55c4da54caca6b51d2944e7554758e
FellypeGC <gabrielfellypesteve@gmail.com>
2024-10-31 15:48:51 -0300

    while e do while

---
+++ modulo6/aula12, 12/condicionais.js
@@ -0,0 +1,13 @@
+let numero = 1;
+
+while(numero <= 5) {
+  console.log(`Número: ${numero}`);
+  numero++;
+}
+
+let funcionariosRegistrados = 0;
+
+while (funcionariosRegistrados < 3) {
+  console.log(`Funcionário ${funcionariosRegistrados + 1} registrou suas horas`);
+  funcionariosRegistrados++;
+}
---
+++ modulo6/aula16, 16/condicionais.js
@@ -0,0 +1,14 @@
+let numero = 1;
+
+do {
+  console.log(`Número ${numero}`);
+  numero++;
+} while(numero <= 5);
+
+
+let funcionariosParaRegistrar = 6;
+
+do {
+  console.log(`Registrando a entrada do funcionário ${funcionariosParaRegistrar}`);
+  funcionariosParaRegistrar--;
+} while(funcionariosParaRegistrar > 0);
