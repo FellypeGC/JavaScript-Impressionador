b828e33897efb33f19d9821785082d137df47963
FellypeGC <gabrielfellypesteve@gmail.com>
2025-01-20 10:16:01 -0300

    forEach corrigido

--- modulo18/forEachExs/farmacia.js
+++ modulo18/forEachExs/farmacia.js
@@ -1,7 +1,7 @@
 const medicamentos = [
   { nome: "Paracetamol", estoque: 150, preco: 5.99 },
-  { nome: "Amoxicilina", estoque: 80, preco: 9.5 },
-  { nome: "Ibuprofeno", estoque: 200, preco: 7.8 },
+  { nome: "Amoxicilina", estoque: 0, preco: 9.5 },
+  { nome: "Ibuprofeno", estoque: 0, preco: 7.8 },
 ];
 
 medicamentos.forEach((medicamento) => {
@@ -17,20 +17,27 @@ medicamentos.forEach((medicamento) => {
   const li = document.createElement('li');
   ulPrice.appendChild(li);
 
-  li.textContent = `Nome: ${medicamento.nome} Preço ${medicamento.preco}`;
+  li.textContent = `Nome: ${medicamento.nome} Preço R$${medicamento.preco.toFixed(2)}`;
 });
 
 
 let medicamentosEmFalta = 0;
+const nomesMedicamentosEmFalta = []; // Array para armazenar os nomes em falta
 
 medicamentos.forEach((medicamento) => {
-  const lackStock = document.querySelector('.lack');
-
   if (medicamento.estoque === 0) {
     medicamentosEmFalta += 1;
-    lackStock.innerHTML += `Nome dos medicamento ${medicamento.nome}`
-    lackStock.innerHTML += `<p>Total de medicamentos faltando: ${medicamentosEmFalta}</p>`;
+    nomesMedicamentosEmFalta.push(medicamento.nome); // Adicionar os nomes ao Array
   } 
+
+  const totalLackStock = document.querySelector('.total-lack');
+  totalLackStock.textContent = `Total de medicamentos em falta: ${medicamentosEmFalta}`;
+  const lackStock = document.querySelector('.lack');
+  lackStock.textContent = `Medicamentos em Falta: ${nomesMedicamentosEmFalta}`;
+
+  if (medicamentosEmFalta > 0) {
+    lackStock.textContent = `Medicamentos em falta: ${nomesMedicamentosEmFalta.join(', ')}`
+  } else {
+    nomesMedicamentosEmFalta.textContent = `Todos os medicamentos estão disponíveis!`
+  }
 });
-
-
