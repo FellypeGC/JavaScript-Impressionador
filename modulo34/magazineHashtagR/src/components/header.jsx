0c478607fe75070e773807f19f049087c6602ec0
FellypeGC <gabrielfellypesteve@gmail.com>
2025-02-10 13:35:41 -0300

    ProductsContainer e ProductCard

--- modulo34/magazineHashtagR/src/components/header.jsx
+++ modulo34/magazineHashtagR/src/components/header.jsx
@@ -3,7 +3,7 @@ import UserButtons from "./UserButtons";
 
 const Header = () => {
   return (
-    <header className="flex text-xl sticky top-0 shadow-xl shadow-slate-400 bg-slate-950 text-slate-200 px-8 py-4 items-end justify-between">
+    <header className="flex text-xl sticky top-0 shadow-xl shadow-slate-400 bg-slate-950 text-slate-200 px-8 py-4 items-end justify-between z-10">
       <a href="/">
         <img className="h-16 p-2" src={logo} alt="Logo da Hashtag" />
       </a>
--- modulo34/magazineHashtagR/src/pages/HomePage/MainSection.jsx
+++ modulo34/magazineHashtagR/src/pages/HomePage/MainSection.jsx
@@ -1,8 +1,10 @@
+import ProductsContainer from "./ProductsContainer";
+
 const MainSection = () => {
   return (
     <>
       <h1>Filtros</h1>
-      <h1>Container Produtos</h1>
+      <ProductsContainer />
     </>
   )
 };
