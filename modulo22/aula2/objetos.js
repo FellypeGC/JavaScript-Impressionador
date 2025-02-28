34eab1cf91d19bee171e7993c432d6f33a97dcc0
FellypeGC <gabrielfellypesteve@gmail.com>
2025-02-28 00:20:13 -0300

    aula 4 & palavra "sessions" corrigida na aula 2.

--- modulo22/aula2/objetos.js
+++ modulo22/aula2/objetos.js
@@ -3,7 +3,7 @@ const student = {
   age: 28,
   modality: "Musculação",
   active: true,
-  sesssions: 12, // Quantidade de sessões restantes
+  sessions: 12, // Quantidade de sessões restantes
   progress: ["Treino A", "Treino B", "Treino C"],
   personalTrainer: {
     name: "João Santos",
@@ -18,9 +18,9 @@ console.log(student.personalTrainer.name);
 
 // Adiciona métodos - funcionalidades
 student.useSession = function () {
-  if (this.sesssions > 0) {
-    this.sesssions -= 1; // Reduza 1
-    console.log("Sessão utilizada: " + `Sessões restantes ${this.sesssions}`);
+  if (this.sessions > 0) {
+    this.sessions -= 1; // Reduza 1
+    console.log("Sessão utilizada: " + `Sessões restantes ${this.sessions}`);
   } else {
     console.log("Não há mais sessões restantes. Renove o seu plano!");
   }
