78a77452808e64681dea786ec6d36e246f7aaf5b
FellypeGC <gabrielfellypesteve@gmail.com>
2025-03-13 11:27:51 -0300

    botão adicionado

--- modulo22/pizzaria-interativa/js/app.js
+++ modulo22/pizzaria-interativa/js/app.js
@@ -1,6 +1,7 @@
 import { Form } from "./Form.js";
 import { Input } from "./Input.js"
 import { Label } from "./Label.js"
+import { Button } from "./Button.js"
 
 document.addEventListener("DOMContentLoaded", () => {
   // Criar formulário 
@@ -10,21 +11,42 @@ document.addEventListener("DOMContentLoaded", () => {
   // Adicionar campos/filhos ao formulario
   const nameLabel = new Label("Nome: ");
   const nameInput = new Input("text", "Digite o seu nome");
+  nameInput.element.id = "name";
 
   form.addChild(nameLabel);
   form.addChild(nameInput);
 
   const pizzaLabel = new Label("Escolha seu sabor de Pizza: ");
   const pizzaInput = new Input("text", "Ex: Calabresa. Manguerita...");
+  pizzaInput.element.id = "pizza";
 
   form.addChild(pizzaLabel);
   form.addChild(pizzaInput);
 
   const quantityLabel = new Label("Quantidade: ");
   const quantityInput = new Input("number", "Ex: 2, 3, 1/2...");
+  quantityInput.element.id = "quantity";
 
   form.addChild(quantityLabel);
   form.addChild(quantityInput);
+
+  // Criar e adicionar o botão de envio
+  const submitButton = new Button("Fazer Pedido");
+  form.addChild(submitButton)
+
+  // Evento de submissão do formulário
+  form.element.addEventListener("submit", (event) => {
+    event.preventDefault(); // Evita o recarregamento da página
+    const pedido = {
+      nome: nameInput.element.value,
+      pizza: pizzaInput.element.value,
+      quantidade: quantityInput.element.value,
+    };
+
+    // Salvar no localStorage
+    localStorage.setItem("ultimoPedido", JSON.stringify(pedido));
+    alert("Pedido salvo com sucesso!");
+  });
 
   // Adicionar o formulario a página
   form.render(document.body);
