606213898e30b2fcbdd2860975d2ce5f86d23733
FellypeGC <gabrielfellypesteve@gmail.com>
2025-03-17 11:00:16 -0300

    Header e UserButtons foram modificados (Header simples e completo + Link Router Dom adicionad).

--- modulo35/magazineHashtagR/src/components/UserButtons.jsx
+++ modulo35/magazineHashtagR/src/components/UserButtons.jsx
@@ -1,5 +1,6 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBagShopping, faUser } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 
 const UserButtons = () => {
   return (
@@ -8,9 +9,9 @@ const UserButtons = () => {
         <FontAwesomeIcon icon={faBagShopping} />
         <div id="cart-amount" className="absolute inline-flex items-center justify-center w-6 h-6 font-bold text-white bg-red-500 border-2 border-white rounded-full -top-2 -right-2">3</div>
       </button>
-      <a>
+      <Link to="/history">
         <FontAwesomeIcon className="px-2" icon={faUser} />
-      </a>
+      </Link>
     </div>
   )
 };
