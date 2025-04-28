89317695798ceb775ef09e3ca0a174cd360c3de8
FellypeGC <gabrielfellypesteve@gmail.com>
2025-04-28 15:28:59 -0300

    Refatoração no contexto do carrinho

--- modulo35/magazineHashtagR/src/App.jsx
+++ modulo35/magazineHashtagR/src/App.jsx
@@ -1,52 +1,20 @@
-import { useState } from 'react';
 import Header from './components/Header';
 import Home from './pages/HomePage/Home';
 import Checkout from './pages/ChekoutPage/Checkout';
 import PurchaseHistory from './pages/PurchaseHistoryPage/PurchaseHistory';
 import { Routes, Route } from 'react-router-dom';
-import { CartContext } from './contexts/CartContext';
+import CartContextProvider from './contexts/CartContext';
 
 const App = () => {
-  const [isCartOpen, setIsCartOpen] = useState(false);
-  const [cartItems, setCartItems] = useState({});
-
-  const addToCart = (productId) => {
-    setCartItems({
-      ...cartItems,
-      [productId]: (cartItems[productId] ?? 0) + 1,
-    });
-  };
-
-  const decreaseUnit = (productId) => {
-    if (cartItems[productId] > 1) {
-      setCartItems({
-        ...cartItems,
-        [productId]: (cartItems[productId]) - 1,
-      });
-    } else {
-      removeFromCart(productId);
-    }
-  };
-
-  const toggleIsCartOpen = () => {
-    setIsCartOpen(!isCartOpen);
-  };
-
-  const removeFromCart = (productId) => {
-    const cartItemsCopy = { ...cartItems };
-    delete cartItemsCopy[productId];
-    setCartItems(cartItemsCopy);
-  }
-
   return (
-    <CartContext.Provider value={ { isCartOpen, toggleIsCartOpen, cartItems, addToCart, decreaseUnit, removeFromCart } }>
+    <CartContextProvider>
       <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/checkout' element={<Checkout />} />
         <Route path='/history' element={<PurchaseHistory />} />
       </Routes>
-    </CartContext.Provider>
+    </CartContextProvider>
   )
 };
 
