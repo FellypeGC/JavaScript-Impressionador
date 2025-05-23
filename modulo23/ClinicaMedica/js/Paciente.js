8e8b032be587b62ffbbe3cfad209157190698e6d
FellypeGC <gabrielfellypesteve@gmail.com>
2025-05-23 16:19:41 -0300

    imports adicionados junto com o metódo

--- modulo23/ClinicaMedica/js/Paciente.js
+++ modulo23/ClinicaMedica/js/Paciente.js
@@ -1,3 +1,5 @@
+import { Pessoa } from "./Pessoa.js";
+
 export class Paciente extends Pessoa {
   constructor(nome, idade, cpf) {
     super(nome, idade, cpf);
--- modulo23/ClinicaMedica/js/main.js
+++ modulo23/ClinicaMedica/js/main.js
@@ -1,3 +1,6 @@
+import { Paciente } from "./Paciente.js";
+import { Medico } from "./Medico.js";
+
 async function carregarDados() {
   const responseMedico = await fetch("./data/medicos.json").then(response => response.json());
   console.log(responseMedico);
@@ -7,3 +10,10 @@ async function carregarDados() {
 }
 
 carregarDados();
+
+const medico = new Medico("Dr. Fernando Dias", 40, "123.455.786-90", "Pediatria");
+const paciente = new Paciente("Fabio Duarte", 10, "123.456.789-65");
+
+medico.agendarConsulta(paciente, "10/05/2025").then((mensagem) => {
+  console.log(mensagem);
+})
