0d6258a013cf668426902bd0366b21d79da630bc
FellypeGC <gabrielfellypesteve@gmail.com>
2025-06-01 11:23:49 -0300

    correção na função drivers em data.js (a.points no bloco if que retorna -1).

--- modulo30/aula_6-20/data.js
+++ modulo30/aula_6-20/data.js
@@ -136,7 +136,7 @@ export const drivers = driversInRandomOrder.sort((b, a) => {
   if (a.points > b.points.points) {
     return 1;
   }
-  if (b.points > a) {
+  if (b.points > a.points) {
     return -1;
   }
   return 0;
