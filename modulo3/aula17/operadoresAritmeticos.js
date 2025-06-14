789903367136c5403b87f570fd5d7cff62f21e96
FellypeGC <gabrielfellypesteve@gmail.com>
2025-06-14 12:07:59 -0300

    módulo 3 operadores aritméticos

---
+++ modulo3/aula17/operadoresAritmeticos.js
@@ -0,0 +1,16 @@
+// Adição +; Subtração -; Multiplicação *; Divisão /; Módulo %; Inversão de sinal/negativo
+
+// Operadores Aritméticos avançados
+// exponenciação
+let base = 2;
+let expoente = 3;
+let resultado = base ** expoente; // 2 elevado a 3 (2³ = 2 * 2 * 2);
+//console.log(resultado);
+
+// Incremento ++ (adicionar o valor 1) Decremento -- (diminuir o valor 1)
+let controle = 0;
+// controle++;
+console.log(--controle);
+console.log(--controle);
+// console.log(controle--);
+// console.log(controle);
