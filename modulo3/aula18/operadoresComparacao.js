5b5d8e492e90817855ded885766c843a4353d95c
FellypeGC <gabrielfellypesteve@gmail.com>
2025-06-14 12:52:28 -0300

    operadores de comparação

---
+++ modulo3/aula18/operadoresComparacao.js
@@ -0,0 +1,15 @@
+// Igualdade e Estrita Igualdade
+console.log(5 == "5");
+console.log(5 === "5"); // boa prática
+// console.log(5 + "5");
+
+console.log(5 != "5");
+console.log(5 !== "5"); // boa prática
+
+// Maior e menor
+console.log(10 <= 20); // true
+
+let senha = "maçã";
+let senhaDigitada = "banana";
+let comparacao = senha === senhaDigitada;
+console.log(`Aqui é a minha senha: ${comparacao}`);
