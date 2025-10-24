e0a98e94eced1a678529773d4886c4f44e6f0705
FellypeGC <gabrielfellypesteve@gmail.com>
2025-10-24 20:52:06 -0300

    Exercícios 4 e 5 módulo 3

--- modulo3/exercicios3/ex.js
+++ modulo3/exercicios3/ex.js
@@ -26,3 +26,38 @@ console.log(produto.nome);
 produto.preco = 165;
 console.log(produto);
 
+// Ex - 4
+let venda = { 
+  cliente: "João",
+  produto: "Bola",
+  valor: 35,
+}
+
+let dataAtual = `${new Date().getDate()}/${new Date().getMonth() + 1}/${new Date().getFullYear()}`;
+
+venda.data = dataAtual;
+delete venda.produto;
+
+console.log(venda);
+
+// Ex - 5
+let estoque = [30, 31, 32, 33];
+let novoEstoque = [...estoque];
+
+novoEstoque[0] = 29;
+
+console.log(novoEstoque); // Output: [ 29, 31, 32, 33 ]
+console.log(estoque); // Output: [ 30, 31, 32, 33 ]
+
+
+let funcionario = {
+  nome: "Lucas",
+  idade: 26,
+}
+
+let novoFuncionario = { ...funcionario }
+
+novoFuncionario.nome = "Jonas";
+
+console.log(novoFuncionario); // Output => nome: 'Jonas'
+console.log(funcionario); // Output => nome: 'Lucas'
