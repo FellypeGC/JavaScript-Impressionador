43625fb919c124968dddcd04e0a1af77c8e72480
FellypeGC <gabrielfellypesteve@gmail.com>
2025-10-25 18:17:33 -0300

    + exercícios (parte 2)

---
+++ modulo3/exercicios2/ex.js
@@ -0,0 +1,38 @@
+// Ex 1 
+console.log("Hello, World!");
+
+// Ex 2
+let msgStr = "Hello, World!";
+console.log(msgStr);
+
+// Ex 3
+let nome = "Jó";
+let idade = 34;
+let isStudent = false;
+console.log({ nome, idade, isStudent });
+
+// Ex 4
+nome = "Daniel";
+idade = 30;
+isStudent = true;
+console.log({ nome, idade, isStudent });
+
+// Ex 5
+const valorProduto = 50;
+console.log(`Valor: R$ ${valorProduto},00`);
+
+// Ex 6
+let endereco;
+let telefone;
+
+// Ex 7
+let nomeProduto = "Notebook";
+let precoProduto = 1500;
+let quantidadeEmEstoque = 20;
+
+console.log({ nomeProduto, precoProduto, quantidadeEmEstoque });
+
+// Ex 8
+let cidade = "São Paulo";
+let populacao = 100_000;
+console.log(`A cidade de ${cidade} tem ${populacao} habitantes.`);
