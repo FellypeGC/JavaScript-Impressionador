ee43aeeef8341637a54b8f006fbcbc16f0a7b8de
FellypeGC <gabrielfellypesteve@gmail.com>
2025-11-01 16:53:19 -0300

    CSS inline

--- modulo34/propsChildren/src/App.jsx
+++ modulo34/propsChildren/src/App.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import { Botao } from "./components/Botao"
 import "./style.css"
+import { Mensagem } from './components/Mensagem'
 
 export const App = () => {
+  const ativo = true;
+
   return (
     <div className='container'>
       <h1>Bem vindo às aulas de CSS no React</h1>
       <button id='botao-principal'>Clique Aqui</button>
+
+      <Botao text="Clique" />
+
+      <hr />
+      <Mensagem>
+        <p style={{ color:ativo ? "green" : "gray" }}>
+          {/* Operador ternário - if/else */}
+          Status: {ativo ? "Ativo" : "Inativo" } 
+        </p>
+      </Mensagem>
     </div>
   )
 }
--- modulo34/propsChildren/src/components/Botao.jsx
+++ modulo34/propsChildren/src/components/Botao.jsx
@@ -4,7 +4,17 @@ export const Botao = ({ text, ...props }) => { //{ text, onClick, type, disabled
   return (
     <> 
       {/* <button onClick={onClick} type={type} disabled={disabled}>{text}</button> */}
-      <button { ...props }>{text}</button>
+      {/* <button { ...props }>{text}</button> */}
+      <button style={
+        { 
+          color: "white", 
+          cursor: "pointer", 
+          padding: "10px", 
+          backgroundColor: "red",
+          fontSize: "16px",
+        }}>
+        {text}
+      </button>
     </>
   )
 }
