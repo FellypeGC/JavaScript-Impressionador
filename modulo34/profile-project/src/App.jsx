454da07e54f35e62162b239a6cd9582145e0dcce
FellypeGC <gabrielfellypesteve@gmail.com>
2025-11-03 10:25:05 -0300

    header module css

--- modulo34/profile-project/src/App.jsx
+++ modulo34/profile-project/src/App.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import { Header } from './components/header/Header'
 
 export const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Header />
+    </>
   )
 }
