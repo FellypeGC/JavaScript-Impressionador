d6bcb8259987046a2206f471bc00607fa20c51ce
FellypeGC <gabrielfellypesteve@gmail.com>
2025-11-07 13:58:32 -0300

    Filtrando e Renderizando

---
+++ modulo35/listas/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { CourseList } from './components/CourseList'
+
+export const App = () => {
+  return (
+    <div>
+      <h1>Renderização de Listas</h1>
+      <CourseList />
+    </div>
+  )
+}
