0b5fbec9f023f69d1d580d11d4c8af7a654ba897
FellypeGC <gabrielfellypesteve@gmail.com>
2025-11-10 18:29:41 -0300

    useEffect - Função de limpeza

--- modulo35/useeffect/src/App.jsx
+++ modulo35/useeffect/src/App.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react'
+import { Contador } from './components/Contador';
 
 export const App = () => {
   const [mensagem, setMensagem] = useState("");
   const [numero, setNumero] = useState(0);
+  const [isShow, setIsShow] = useState(true);
 
   useEffect(() => {
     setMensagem(`O número atual é: ${numero}`);
@@ -23,6 +25,12 @@ export const App = () => {
       <h1>UseEffect</h1>
       <p>{mensagem}</p>
       <button onClick={() => setNumero(numero + 1)}>Aumentar</button>
+
+      <h1>UseEffect - Função Limpeza</h1>
+      <button onClick={() => setIsShow(!isShow)}>
+        {isShow ? "Ocultar Contador" : "Mostrar Contador"}
+      </button>
+      {isShow && <Contador />}
     </>
   )
 }
