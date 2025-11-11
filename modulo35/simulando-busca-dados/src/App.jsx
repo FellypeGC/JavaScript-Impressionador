3d7a270aba647c9d60ac7fc44accbd71b48862b1
FellypeGC <gabrielfellypesteve@gmail.com>
2025-11-11 20:28:39 -0300

    Simulando Busca de Dados

---
+++ modulo35/simulando-busca-dados/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { FetchDataExample } from './components/FetchDataExample'
+
+export const App = () => {
+  return (
+    <>
+      <h1>Simulando Dados</h1>
+      <FetchDataExample />
+    </>
+  )
+}
