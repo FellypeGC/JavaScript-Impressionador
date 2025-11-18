b6c4c80d9b5fc9f87867404d35f6ec35dc795880
FellypeGC <gabrielfellypesteve@gmail.com>
2025-11-18 16:01:45 -0300

    to do list em react: estilo base do App

---
+++ modulo37/to-do-list/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css"
+import './index.css'
+
+export const App = () => {
+  return (
+    <div className="container">
+      <h1 className="app-title">Lista de Tarefas</h1>
+      {/* Componente - Input Botão */}
+      {/* Componente - Lista de componentes */}
+    </div>
+  )
+}
