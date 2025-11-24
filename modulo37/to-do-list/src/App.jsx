8290c1db721bbb90960c617836e1e2019f9ba6ee
FellypeGC <gabrielfellypesteve@gmail.com>
2025-11-24 20:15:46 -0300

    Ação de adicionar tarefas

--- modulo37/to-do-list/src/App.jsx
+++ modulo37/to-do-list/src/App.jsx
@@ -1,13 +1,29 @@
+import { useState } from "react"
 import "./App.css"
 import { AddToDo } from "./components/AddToDo"
 import './index.css'
 
 export const App = () => {
+  const [todos, setTodos] = useState([
+    { id: 1, text: "Exemplo de tarefa", completed: false }, 
+    { id: 2, text: "Aprender React", completed: false }
+  ]);
+
+  const addTodo = (text) => {
+    const newTodo = {
+      id: Date.now(),
+      text: text,
+      completed: false,
+    };
+    setTodos([...todos, newTodo]);
+  }
+
   return (
     <div className="container">
       <h1 className="app-title">Lista de Tarefas</h1>
       {/* Componente - Input Botão */}
-      <AddToDo onAddTodo={() => {}} />
+      <AddToDo onAddTodo={addTodo} />
+        {todos.map((todo) => todo.text)}
       {/* Componente - Lista de componentes */}
     </div>
   )
