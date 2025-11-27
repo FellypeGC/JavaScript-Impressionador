88d9b99684b1b2043c0fadc081a1017ea9b9dc48
FellypeGC <gabrielfellypesteve@gmail.com>
2025-11-27 15:48:04 -0300

    estilização todolist & todoitem

--- modulo37/to-do-list/src/components/TodoItem.jsx
+++ modulo37/to-do-list/src/components/TodoItem.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
+import "./TodoItem.css"
 
 export const TodoItem = ({ todo, onToggle, onDelete }) => {
   return (
-    <div className='todo-content'>
-      <input 
-        type="checkbox" 
-        className='todo-checkbox' 
-        checked={todo.completed} 
-        onChange={() => onToggle(todo.id)} 
-      />
-
-      <span className='todo-text'>{todo.text}</span>
-
-      <button className='delete-button' onClick={() => onDelete(todo.id)}>Remover</button>
+    <div className={`todo-item ${todo.completed ? "completed" : ""}`}>
+      <div className='todo-content'>
+        <input
+          type="checkbox"
+          className='todo-checkbox'
+          checked={todo.completed}
+          onChange={() => onToggle(todo.id)}
+        />
+        <span className='todo-text'>{todo.text}</span>
+        <button className='delete-button' onClick={() => onDelete(todo.id)}>Remover</button>
+      </div>
     </div>
   )
 }
